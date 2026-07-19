7caaa8dbe6a7e8dc4dbd2834792613681cbfd116
rohit-kamble-official <itsrohitrk.official@gmail.com>
2026-07-20 04:14:49 +0530

    Fix Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
+    build: {
     outDir: 'dist',
     rollupOptions: {
       output: {
